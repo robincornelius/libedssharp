01e2893ea385d232687ad1e46748a8d42f84ee64
Robin Cornelius <robin.cornelius@gmail.com>
2016-11-09 16:46:20 +0000

    Allow the use of escape characters for VISIBLE_STRING type. Fixes #19
    Only a subset of all possible escapes are supported but this is :-
    
    \a \b \f \n \r \t \v \0 \\
    
    Note we explicitly do not support \xx or \ooo

--- libEDSsharp/CanOpenNodeExporter.cs
+++ libEDSsharp/CanOpenNodeExporter.cs
@@ -799,19 +799,24 @@ const CO_OD_entry_t CO_OD[");
 
                 case DataType.VISIBLE_STRING:
                     {
-                        string array = "{";
-                        foreach (char s in defaultvalue)
-                        {
-                            array += "'" + s + "'";
 
-                            if (!object.ReferenceEquals(s, defaultvalue.Last()))
-                            {
-                                array += ", ";
-                            }
+                        ASCIIEncoding a = new ASCIIEncoding();
+                        string unescape = StringUnescape.Unescape(defaultvalue);
+                        char[] chars = unescape.ToCharArray();
+
+                        string array = "{";
+
+                        foreach (char c in chars)
+                        {
+
+                            array += "'" + StringUnescape.Escape(c) + "', ";
                         }
+
+                        array = array.Substring(0, array.Length - 2);
 
                         array += "}";
                         return array;
+
                     }
 
 
@@ -856,7 +861,41 @@ const CO_OD_entry_t CO_OD[");
             }
         }
 
-       string make_cname(string name)
+        public static string ParseString(string input)
+        {
+            var provider = new Microsoft.CSharp.CSharpCodeProvider();
+            var parameters = new System.CodeDom.Compiler.CompilerParameters()
+            {
+                GenerateExecutable = false,
+                GenerateInMemory = true,
+            };
+
+            var code = @"
+        namespace Tmp
+        {
+            public class TmpClass
+            {
+                public static string GetValue()
+                {
+                    return """ + input + @""";
+                }
+            }
+        }";
+
+            var compileResult = provider.CompileAssemblyFromSource(parameters, code);
+
+            if (compileResult.Errors.HasErrors)
+            {
+                throw new ArgumentException(compileResult.Errors.Cast<System.CodeDom.Compiler.CompilerError>().First(e => !e.IsWarning).ErrorText);
+            }
+
+            var asmb = compileResult.CompiledAssembly;
+            var method = asmb.GetType("Tmp.TmpClass").GetMethod("GetValue");
+
+            return method.Invoke(null, null) as string;
+        }
+
+        string make_cname(string name)
        {
             if (name == null)
                 return null;
@@ -1110,6 +1149,70 @@ const CO_OD_entry_t CO_OD[");
             }
 
         }
-
     }
+
+
+
+
+    public static class StringUnescape
+    {
+        public static string Escape(char c)
+        {
+
+            switch(c)
+            {
+                case '\0':
+                    return @"\0";
+                case '\n':
+                    return @"\n";
+                case '\r':
+                    return @"\r";
+                case '\t':
+                    return @"\t";
+                case '\a':
+                    return @"\a";
+                case '\b':
+                    return @"\b";
+                case '\f':
+                    return @"\f";
+                case '\v':
+                    return @"\v";
+
+                default:
+                    return c.ToString();
+              
+            }
+
+        }
+
+        public static string Unescape(this string txt)
+        {
+            if (string.IsNullOrEmpty(txt)) { return txt; }
+            StringBuilder retval = new StringBuilder(txt.Length);
+            for (int ix = 0; ix < txt.Length;)
+            {
+                int jx = txt.IndexOf('\\', ix);
+                if (jx < 0 || jx == txt.Length - 1) jx = txt.Length;
+                retval.Append(txt, ix, jx - ix);
+                if (jx >= txt.Length) break;
+                switch (txt[jx + 1])
+                {
+                    case 'n': retval.Append('\n'); break;  // Line feed
+                    case 'r': retval.Append('\r'); break;  // Carriage return
+                    case 't': retval.Append('\t'); break;  // Tab
+                    case '0': retval.Append('\0'); break;  // Null
+                    case 'a': retval.Append('\a'); break;  // Bell
+                    case 'b': retval.Append('\b'); break;  // Backspace
+                    case 'f': retval.Append('\f'); break;  // Formfeed
+                    case 'v': retval.Append('\v'); break;  // Verticle tab
+                    case '\\': retval.Append('\\'); break; // Don't escape
+                    default:                                 // Unrecognized, copy as-is
+                        retval.Append('\\').Append(txt[jx + 1]); break;
+                }
+                ix = jx + 2;
+            }
+            return retval.ToString();
+        }
+    }
+
 }
