ee4a64c0c8d91537b09a5c53f3c95db33a547274
Robin Cornelius <robin.cornelius@gmail.com>
2016-11-21 20:16:11 +0000

    Add some missing GPL headers

--- EDSTest/MaxSubIndexFrm.cs
+++ EDSTest/MaxSubIndexFrm.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+    This file is part of libEDSsharp.
+
+    libEDSsharp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libEDSsharp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libEDSsharp.  If not, see <http://www.gnu.org/licenses/>.
+ 
+    Copyright(c) 2016 Robin Cornelius <robin.cornelius@gmail.com>
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
