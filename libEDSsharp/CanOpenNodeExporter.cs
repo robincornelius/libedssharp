a760324de96a9a92adbab535e591e158cb49e722
Martin Wagner <martin.wagner@neuberger.net>
2017-01-24 14:46:36 +0100

    change license...
    according to https://github.com/robincornelius/libedssharp/issues/47

--- libEDSsharp/CanOpenNodeExporter.cs
+++ libEDSsharp/CanOpenNodeExporter.cs
@@ -179,25 +179,43 @@ namespace libEDSsharp
    <http://canopennode.sourceforge.net>
 
    (For more information see <CO_SDO.h>.)
-*/
-/*
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
 
+   This file is part of CANopenNode, an opensource CANopen Stack.
+   Project home page is <https://github.com/CANopenNode/CANopenNode>.
+   For more information on CANopen see <http://www.can-cia.org/>.
+ 
+   CANopenNode is free and open source software: you can redistribute
+   it and/or modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation, either version 2 of the
+   License, or (at your option) any later version.
+  
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-   Original Author: Janez Paternoster
-
-
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+  
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+  
+   Following clarification and special exception to the GNU General Public
+   License is included to the distribution terms of CANopenNode:
+  
+   Linking this library statically or dynamically with other modules is
+   making a combined work based on this library. Thus, the terms and
+   conditions of the GNU General Public License cover the whole combination.
+  
+   As a special exception, the copyright holders of this library give
+   you permission to link this library with independent modules to
+   produce an executable, regardless of the license terms of these
+   independent modules, and to copy and distribute the resulting
+   executable under terms of your choice, provided that you also meet,
+   for each linked independent module, the terms and conditions of the
+   license of that module. An independent module is a module which is
+   not derived from or based on this library. If you modify this
+   library, you may extend this exception to your version of the
+   library, but you are not obliged to do so. If you do not wish
+   to do so, delete this exception statement from your version.
+ 
    This file was automatically generated with libedssharp Object
    Dictionary Editor. DON'T EDIT THIS FILE MANUALLY !!!!
 
