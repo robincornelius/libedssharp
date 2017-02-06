2b2f0fd9a54a0cd1d12bcf3687a993d83aec8e4a
Robin Cornelius <robin.cornelius@gmail.com>
2017-02-06 10:38:50 +0000

    Add some unit tests, first test check cname exporter is working

---
+++ Tests/ExporterTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using libEDSsharp;
+
+namespace Tests
+{
+    [TestClass]
+    public class ExporterTests : CanOpenNodeExporter
+    {
+        [TestMethod]
+        public void Test_cname_conversion()
+        {
+
+            if (make_cname("axle 0 wheel right controlword") != "axle0WheelRightControlword")
+                throw (new Exception("make_cname Conversion error"));
+
+            if (make_cname("mapped object 4") != "mappedObject4")
+                throw (new Exception("make_cname Conversion error"));
+
+            if (make_cname("COB ID used by RPDO") != "COB_IDUsedByRPDO")
+                throw (new Exception("make_cname Conversion error"));
+
+        }
+    }
+}
