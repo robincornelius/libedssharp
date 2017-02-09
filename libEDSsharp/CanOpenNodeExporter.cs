9b2e9eff90c948b175c1225037a2ccf63b7c0fbc
Robin Cornelius <robin.cornelius@gmail.com>
2017-02-09 20:34:07 +0000

    Do not issue subindex warning for OD 0x1003, fixes #63

--- libEDSsharp/CanOpenNodeExporter.cs
+++ libEDSsharp/CanOpenNodeExporter.cs
@@ -646,9 +646,9 @@ const CO_OD_entry_t CO_OD[");
                 //Arrays really should obey the max subindex paramater not the physical number of elements
                 if (od.objecttype == ObjectType.ARRAY)
                 {
-                    if (od.getmaxsubindex() != nosubindexs)
+                    if ((od.getmaxsubindex() != nosubindexs) && od.index != 0x1003) //ignore this warning on 0x1003 it is a special case
                     {
-                        Warnings.warning_list.Add(String.Format("Subindex descripency on object 0x{0:x4} arraysize: {1} vs max-subindex: {2}", od.index, nosubindexs, od.getmaxsubindex()));
+                        Warnings.warning_list.Add(String.Format("Subindex discrepancy on object 0x{0:x4} arraysize: {1} vs max-subindex: {2}", od.index, nosubindexs, od.getmaxsubindex()));
                     }
                     nosubindexs = od.getmaxsubindex();
                 }
