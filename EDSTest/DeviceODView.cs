230a21384ed1f62ad96fe36bca2792a4e6d94d0a
Robin Cornelius <robin.cornelius@gmail.com>
2017-12-09 19:45:42 +0000

    Correctly replace \n with \r\n so that descriptions format nicely

--- EDSTest/DeviceODView.cs
+++ EDSTest/DeviceODView.cs
@@ -28,6 +28,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using libEDSsharp;
 using System.Reflection;
+using System.Text.RegularExpressions;
 
 
 namespace ODEditor
@@ -266,7 +267,7 @@ namespace ODEditor
             if (od.Description == null)
                 textBox_description.Text = "";
             else
-                textBox_description.Text = od.Description.Replace("(?<!\r)\n", "\r\n");
+                textBox_description.Text = Regex.Replace(od.Description, "(?<!\r)\n", "\r\n");
 
             comboBox_pdomap.SelectedItem = od.PDOtype.ToString();
 
