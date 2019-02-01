821eedb2194dee01e1e5227c5c9196cc9f9e1b8c
Robin Cornelius <robin.cornelius@gmail.com>
2019-02-01 20:56:45 +0000

    Update copyright header

--- libEDSsharp/CanOpenXDD.cs
+++ libEDSsharp/CanOpenXDD.cs
@@ -1,4 +1,26 @@
-﻿using System;
+﻿/*
+    This file is part of libEDSsharp.
+
+    libEDSsharp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libEDSsharp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libEDSsharp.  If not, see <http://www.gnu.org/licenses/>.
+ 
+    Copyright(c) 2016 - 2019 Robin Cornelius <robin.cornelius@gmail.com>
+    based heavily on the files CO_OD.h and CO_OD.c from CanOpenNode which are
+    Copyright(c) 2010 - 2016 Janez Paternoster
+*/
+
+
+using System;
 using System.Xml.Serialization;
 using System.IO;
 using XSDImport;
